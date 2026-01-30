e3416b6280360dd14a93cd93a46c7b8a9d94c4a0
Timothy Bjorklund <timothybjorklund@outlook.com>
2026-01-30 13:14:51 -0700

    Initialize Module 3 CT project with TODO planning
    - Created initial C++ console project for M3 CT
    - Added TODO list to outline program requirements and development steps

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : P3_M3CT_IntegerPointersProgram
+// Author      : Timothy Bjorklund
+//============================================================================
+
+/* ===========Project Planning===========
+ * DONE: Project set up
+ * DONE: Local GIT Repository
+ * DONE: Create GitHub Repository
+ * DONE: Initial Commit and Push to GitHub with ScreenShot (Project + Commit)
+ * DONE: Initial pseudocode brainstorm
+ * TODO: Prompt user to enter three int values
+ * TODO: Store each entered int into three separate var
+ * TODO: Set up 3 int pointers and initialize them to nullptr
+ * TODO: Allocate dynamic memory for pointers
+ * TODO: Store each var value into its allocated memory
+ * TODO: Cout - each var to the console
+ * TODO: Cout - each pointers dereferenced value
+ * TODO: Cout - pointer addresses
+ * TODO: Free the allocated memory (delete operator)
+ * TODO: Test program (Screenshot Code and Results)
+ * TODO: Commit (Screenshot)
+ * TODO: Push to GitHub
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "IntegerPointersProgram Start...." << endl;
+}
